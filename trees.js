5c8eca3e5cb86899b4cc72a40996c6772e9a2fa7
Rodrigo <rope.50@gmail.com>
2016-09-21 18:04:58 -0300

    [tree] search node

--- trees.js
+++ trees.js
@@ -1,3 +1,8 @@
+/**
+* Multiple Operations using trees
+*
+*/
+
 function Tree(){
 	this.root = null;
 }
@@ -75,19 +80,91 @@ Tree.prototype = {
 		}
 		leafs(this.root);
 	},
-	deleteNode : function(node){
+	deleteNode : function(value){
+		var node  =  this.searchNode(value);
+		if(node === null){
+			return;
+		}
+		else if(!node.left && !node.right){
+			//only remove de node
+		}
+		else if(node.left && node.right){
 
+		}
+		else if(node.left && !node.right){
+
+		}
+		else if(!node.left && node.right){
+
+		}
 	},
 	searchNode: function(value){
-		
+		var search = function(n){
+			if(n.value === value)
+				return n;
+			else if(value < n.value){
+				if(n.left)
+					return search(n.left);
+				else
+					return null;
+			}
+			else{
+				if(n.right)
+					return search(n.right);
+				else
+					return null;
+			}
+		};
+		return search(this.root);
 	},
+	getPaths : function(){
+		var pathsArr = [];
+		var paths = function(n,path){
+			if(!n) return;
+			path = path ? path : "";
+			if(path.length)
+				path += "->"+n.value;
+			else
+				path = ""+n.value;
+			if(!n.left && !n.right){
+				pathsArr.push(path);
+				//console.log(n.value, path, paths);
+			}
+			else{
+				if(n.left)
+					paths(n.left, path);
+				if(n.right)
+					paths(n.right, path);	
+			}
+		};
+		paths(this.root);
+		return pathsArr;
+	},
+	validate: function(){
+		var validate =  function(n){
+			if(!n) return true;
+			if(n.left){
+				if(n.left.value > n.value){ 
+					return true;
+				}
+				return validate(n.left);
+			}
+			if(n.right){
+				if(n.right.value < n.value ){
+					return true;
+				}
+				return validate(n.right);
+			}
+		}
+		return !validate(this.root);
+	}
 }
 
-arr  = [100,50,25,75,12,18,15,200,150,300,175,180,160];
+arr  = [100, 50, 25, 75, 12, 18, 15, 200, 150, 300, 175, 180, 160];
 
 tree = new Tree();
 tree.fill(arr);
 //tree.leafs();
 //tree.preOrder();
 //tree.postOrder();
-console.log(tree);
+console.log(tree.validate());
