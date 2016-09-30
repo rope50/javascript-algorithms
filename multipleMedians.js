52c9b3a636c9fe2cd7b5130b7fe9c19b60ff77d4
Rodrigo Urbina <rodrigo@Rodrigo.local>
2016-09-30 10:44:11 -0300

    add multipleMedians.js and trie.js

---
+++ multipleMedians.js
@@ -0,0 +1,39 @@
+var Heap = require('./heap.js'); //Require our implementation of Heaps!
+
+function fillHeaps(list){
+	maxHeap = new Heap(Heap.TYPE_MAX);
+	minHeap = new Heap(Heap.TYPE_MIN);
+	for(var i = 0; i < list.length; i++){
+		if(!(i%2))
+			maxHeap.insert(list[i]);
+		else
+			minHeap.insert(list[i]);
+		balanceHeaps(minHeap, maxHeap);
+		median = findMedian(minHeap, maxHeap);
+		console.log(median.toFixed(1));
+	}
+}
+function findMedian(minHeap, maxHeap){
+ 	if(maxHeap.size > minHeap.size)
+ 		return maxHeap.items[0];
+ 	else if(minHeap.size > maxHeap.size)
+ 		return minHeap.items[0];
+ 	else
+ 		return (minHeap.items[0] + maxHeap.items[0]) / 2;
+}
+
+function balanceHeaps(minHeap, maxHeap){
+	if(!minHeap.size || !maxHeap.size)
+		return;
+	while(minHeap.items[0] < maxHeap.items[0]){
+		temp = minHeap.items[0];
+		minHeap.items[0] = maxHeap.items[0];
+		maxHeap.items[0] = temp;
+
+		maxHeap.heapifyDown();
+		minHeap.heapifyDown();
+	}
+}
+
+var list = [12,4,5,3,8,7];
+fillHeaps(list);
---
+++ trie.js
@@ -0,0 +1,61 @@
+Node =  function(key){
+	this.key = key;
+	this.value = 0;
+	this.complete = false;
+	this.children = [];
+}
+
+Trie = function(){
+	this.root = new Node('.');
+}
+
+Trie.prototype = {
+	add: function(word){
+		var p = this.root, key;
+		for(var i = 0 ; i < word.length; i++){
+			key = word[i];
+			if(p.children[key.charCodeAt(0)]){
+				p = p.children[key.charCodeAt(0)];
+			}
+			else{
+				node = new Node(key);
+				p.children[key.charCodeAt(0)] = node;
+				p = p.children[key.charCodeAt(0)];
+			}
+			p.value++;
+			if(i === word.length - 1){
+				p.complete = true;	
+			}
+		}
+	},
+	search: function(word){
+		var p = this.root, key;
+		for(var i = 0 ; i < word.length; i++){
+			key = word[i];
+			if(!p.children[key.charCodeAt(0)]){
+				console.log(0);
+				return 0;
+			}
+			p = p.children[key.charCodeAt(0)];
+		}
+		console.log(p.value);
+		return p.value;
+	},
+	operate : function(op, word){
+		if(op === 'add'){
+			this.add(word);
+		}
+		else if(op === 'find'){
+			this.search(word);
+		}
+	}
+}
+
+
+var trie = new Trie();
+trie.add('cam');
+trie.add('camera');
+trie.add('cameration');
+
+trie.search('');
+
