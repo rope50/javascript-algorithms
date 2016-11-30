755849dee24f0d31dfb5fa0bfffd64bb066a2a90
Rodrigo <rope.50@gmail.com>
2016-11-30 18:25:49 -0300

    dollars.js progress

--- dollars.js
+++ dollars.js
@@ -6,3 +6,20 @@ Encontrar e imprimir el numero de diferentes maneras en que puedas hacer cambio 
 si cada moneda esta disponible en una cantidad infinita.
 
 */
+var dDict = {};
+var dolars = function(n,c){
+	var acum = 0
+	for(var i in c){
+		if(n - c[i] >=0){
+			acum = acum + 1 + dolars(n-c[i],c);
+		}
+		else{
+			acum += dolars(n-c[i],c);
+		}
+	}
+	return acum;
+}
+
+var n = 4;
+var c= [1,2,3]
+console.log(dolars(n,c));
